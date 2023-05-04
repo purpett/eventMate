ef8476cb52af892d23652d25929e068d582bbe91
Michela Bicocchi <mbicocchi1@gmail.com>
2023-05-04 19:38:37 +0100

    Create User model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  attending: { type: Schema.Types.ObjectId, ref: 'Event' },
+  // add created events?
+  // add liked/saved events?
+  // add profile image?
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
