964138f10ad666a7f935f598732f7168b120d8ab
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-05-05 14:54:10 +0100

    Add body, method and headers to createUser api function

--- eventmate-frontend/src/apis/UserApis.js
+++ eventmate-frontend/src/apis/UserApis.js
@@ -4,11 +4,27 @@ export const GetUser = (id) => {
 }
 
 // Create single User
-export const CreateUser = () => {
-  return fetch(`http://localhost:5002/api/users/`)
+export const CreateUser = (userInfo) => {
+  return fetch(`http://localhost:5002/api/users/`, {
+    method: 'POST',
+    header: {
+      "Content-Type": "application/json",
+      "Accept": "application/json", 
+    },
+    body: JSON.stringify({
+      username: `${userInfo.username}`,
+      password: `${userInfo.password}`,
+      attending: []
+    })
+  })
 }
 
 // Delete single User
 export const DeleteUser = (id) => {
+  return fetch(`http://localhost:5002/api/users/${id}`)
+}
+
+// Update single User
+export const UpdateUser = (id) => {
   return fetch(`http://localhost:5002/api/users/${id}`)
 }
