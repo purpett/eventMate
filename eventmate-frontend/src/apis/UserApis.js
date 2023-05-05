4e7147c6f02b94d7c023ccb73f49def8341ec84e
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-05-05 15:10:11 +0100

    add method, body and headers to UpdateUser api function

--- eventmate-frontend/src/apis/UserApis.js
+++ eventmate-frontend/src/apis/UserApis.js
@@ -28,6 +28,18 @@ export const DeleteUser = (id) => {
 }
 
 // Update single User
-export const UpdateUser = (id) => {
-  return fetch(`http://localhost:5002/api/users/${id}`)
+export const UpdateUser = (id, userChanges, userInfo) => {
+  return fetch(`http://localhost:5002/api/users/${id}`, {
+    method: `PUT`,
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      username: `${userChanges.username? userChanges.username: userInfo.username}`,
+      password: `${userChanges.password? userChanges.password: userInfo.password}`,
+      attending: [...userInfo.attending, userChanges.attending]
+    })
+  }
+  )
 }
