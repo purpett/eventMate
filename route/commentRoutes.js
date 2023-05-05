2cd09182cc96c07a0d138fec3ae240654137fd20
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-05-05 12:09:48 +0100

    fix local problems

---
+++ route/commentRoutes.js
@@ -0,0 +1,79 @@
+const express = require('express')
+
+const Event = require('../models/event')
+
+
+const router = express.Router()
+
+/*
+Action: CREATE
+Method: Post
+URI: /api/events/:id/comment
+Description: Create a new Comment
+*/
+
+
+
+
+
+router.post('/api/events/:id/comments', (req, res) => {
+    Event.findById(req.params.id)
+      .then((event) => {
+        const comment = req.body
+        event.comments.push(comment);
+        event.save()
+          .then((event) => {
+            res.status(201).json(event)
+          })
+      })
+  })
+
+
+
+
+
+
+
+
+/*
+Action: DESTROY
+Method: Delete
+URI: /api/events/:id/comments/:id
+Description: Delete an Comment by its Comment ID
+ */
+
+router.delete('/api/events/:id/comments/:commentId', (req, res) => {
+    Event.findById(req.params.id)
+        .then((event) => {
+            const commentId = req.params.commentId
+            event.comments.id(commentId).deleteOne();
+            event.save()
+                .then((event) => {
+                    res.status(201).json(event)
+                })
+        })
+})
+
+/*
+Action: UPDATE
+Method: Put/Patch
+URI: /api/comment/e585b9q283u49
+Description: Update an Comment by its ID
+*/
+
+router.put('/api/events/:id/comments/:commentId', (req, res) => {
+    Event.findById(req.params.id)
+        .then((event) => {
+            const commentId = req.params.commentId
+
+            const comment = event.comments.id(req.params.commentId)
+            console.log(comment);
+            comment.text = req.body.text;
+            event.save()
+                .then((event) => {
+                    res.status(201).json(event)
+                })
+        })
+})
+
+module.exports = router
