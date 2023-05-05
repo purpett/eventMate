54df540fbb5ffed71eeff850dab104100171bf88
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-05-05 14:44:31 +0100

    Add create user api call in front end

--- eventmate-frontend/src/apis/UserApis.js
+++ eventmate-frontend/src/apis/UserApis.js
@@ -3,3 +3,9 @@ export const GetUser = (id) => {
   return fetch(`http://localhost:5002/api/users/${id}`)
 }
 
+// Create single User
+export const CreateUser = () => {
+  return fetch(`http://localhost:5002/api/users/`)
+}
+
+
