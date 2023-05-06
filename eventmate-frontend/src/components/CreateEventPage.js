d79fc686854287f83e680802c9fdad57e79dd3f7
Miles Morris <milesmorris123@gmail.com>
2023-05-06 17:11:56 +0100

    added comments to create event page

--- eventmate-frontend/src/components/CreateEventPage.js
+++ eventmate-frontend/src/components/CreateEventPage.js
@@ -3,40 +3,41 @@ import { createEvent } from "../apis/EventApis";
 import { useState } from "react"
 
 export default function CreateEventPage() {
+
+  //create a state to hold the values from the input fields
   const [createdEvent, setCreatedEvent] = useState({
   title: "",
   description: "",
   location: "",
   date: "",
-  // organiser: "Miles",
   comments: [],
   attendees: []
 
   })
   
+  // function to call the post create event api with the new state which has just been set 
   function createOneEvent() {
     createEvent(createdEvent)
       .then((event) => event.json())
       .then((data => console.log(data)))
 
-      console.log(createdEvent)
+      // console.log(createdEvent)
   }
 
+  // function to get the values from the input fields and map those into the relevant state object fields
   function handleTextInput (e) {
     setCreatedEvent({ ...createdEvent, [e.target.name]: e.target.value });
-    console.log(createdEvent);
+  //   console.log(createdEvent);
   }
 
   return (
     <div>
+      <h1>Create A New Event</h1>
       <form>
         <input name='title' onChange={handleTextInput} placeholder="Enter your Events Title"></input>
         <input name='location' onChange={handleTextInput} placeholder="Location"></input>
         <input name='date' onChange={handleTextInput} placeholder="Date"></input>
         <input name='description' onChange={handleTextInput} placeholder="Description"></input>
-
-
-
         <button onClick={(e) => {
           e.preventDefault();
           createOneEvent()
