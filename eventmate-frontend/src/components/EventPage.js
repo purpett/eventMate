98a52052eb934f0b6e70e9a58d24151a07de7788
Michela Bicocchi <mbicocchi1@gmail.com>
2023-05-09 19:09:44 +0100

    Add edit event functionality

--- eventmate-frontend/src/components/EventPage.js
+++ eventmate-frontend/src/components/EventPage.js
@@ -10,9 +10,13 @@ import { updateUser } from "../apis/UserApis"
 export default function EventPage() {
   // State to store the information about the event. Will store an object after the page is loaded
   const [singleEvent, setSingleEvent] = useState({})
+  const [editedEvent, setEditedEvent] = useState(singleEvent)
 
   // This state is used as a switch for the create comment form
-  const [showForm, setShowForm] = useState(false)
+  const [showCommentForm, setShowCommentForm] = useState(false)
+
+  // This state is used as a switch for the edit event form
+  const [showEventForm, setShowEventForm] = useState(false)
 
   // Grab the event Id from the url and store it in the variable called id.
   const { id } = useParams()
@@ -40,17 +44,12 @@ export default function EventPage() {
   }
 
   // Function to update one event. its fed the Id and the updated event information
-
-  // For testing purposes
-  const updatedEvent = {
-    title: 'Another event'
-  }
-
   // changed so that it updates with any new info (even multiple at once )
   function updateOneEvent() {
-    updateEvent(id, updatedEvent)
+    updateEvent(id, editedEvent)
       .then((event) => event.json())
       .then((data => setSingleEvent(data)))
+      .catch((error) => console.log(error.message))
   }
 
   function addEventIdToUser() {
@@ -64,32 +63,60 @@ export default function EventPage() {
     updateUser(payloadFromToken.userId, changes)
       .then((results) => results.json())
     // .then((data) => console.log(data))
+  }
 
+  function handleInputOnChange(e) {
+    setEditedEvent({ ...editedEvent, [e.target.name]: e.target.value })
+  }
+
+  function toggleForm() {
+    setShowEventForm(!showEventForm)
   }
 
   return (
     <div>
-      <div>
+      {!showEventForm && <div>
         {/* Checks to see if the fetch request is complete before showing the event information */}
-        <p>Title: {singleEvent ? singleEvent.title : null}</p>
-        <p>Description: {singleEvent ? singleEvent.description : null}</p>
-        <p>Location: {singleEvent ? singleEvent.location : null}</p>
-        <p>Date: {singleEvent ? singleEvent.date : null}</p>
-        <p>Organiser: {singleEvent ? singleEvent.organiser : null}</p>
+        <p>Title: {singleEvent.title}</p>
+        <p>Description: {singleEvent.description}</p>
+        <p>Location: {singleEvent.location}</p>
+        <p>Date: {singleEvent.date}</p>
+        <p>Organiser: {singleEvent.organiser}</p>
         <p>Tags:</p>
-        <button>Like/Fav</button>
-        {/* Button that toggles the showForm boolean state */}
-        <button
-          onClick={() => setShowForm(!showForm)}
-        >Comment</button>
-        <button onClick={addEventIdToUser}>I Want to Attend</button>
+        {/* <button>Like/Fav</button> */}
+        {/* Button that toggles the showCommentForm boolean state */}
+        <button onClick={() => setShowCommentForm(!showCommentForm)}>Comment</button>
+        <button onClick={addEventIdToUser}>Attend</button>
+        <button onClick={toggleForm}>Edit Event</button>
+        <button onClick={deleteOneEvent}>Delete Event</button>
         <hr />
-      </div>
-      <button onClick={deleteOneEvent}>Delete Event</button>
-      <button onClick={updateOneEvent}>Update Event</button>
+      </div>}
+      {showEventForm && <form onSubmit={updateOneEvent}>
+        <input
+          name='title'
+          onChange={handleInputOnChange}
+          placeholder={singleEvent.title}
+        />
+        <input
+          name='location'
+          onChange={handleInputOnChange}
+          placeholder={singleEvent.location}
+        />
+        <input
+          name='date'
+          onChange={handleInputOnChange}
+          placeholder={singleEvent.date}
+        />
+        <input
+          name='description'
+          onChange={handleInputOnChange}
+          placeholder={singleEvent.description}
+        />
+        <button type="submit">Save changes</button>
+      </form>}
 
-      {/* If the showForm is true to Comment form will appear and pass down the id of the event. */}
-      {showForm && <CreateCommentForm
+      {/* If the showCommentForm is true to Comment form will appear and pass down the id of the event. */}
+      {showCommentForm && <CreateCommentForm
         setSingleEvent={setSingleEvent}
         id={id} />}
       {/* This will be where the comments will be generated. The whole event information is passed down */}
