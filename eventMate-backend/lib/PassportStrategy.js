895a7d80f0dc90c33f42dbf9aeba9bf067936d15
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-05-09 09:42:42 +0100

    Set up lib folder with passportOptions and passportStrategy

---
+++ eventMate-backend/lib/PassportStrategy.js
@@ -0,0 +1,23 @@
+// Passport Strategy Package
+const passportJWT = require('passport-jwt')
+const User = require('../models/user')
+// Passport Options
+const jwtOptions = require('./passportOptions')
+
+// JSON Web Token Strategy object that we will be using
+const JwtStrategy = passportJWT.Strategy
+
+// This `strategy` function is going to be called every time a user makes a request to our API with a JSON Web Token
+
+// This function is where we are going to see if the requesting user has a valid jWT or not. And, to see if the token is expired.
+const strategy = new JwtStrategy(jwtOptions, (jwtPayload, next) => {
+  console.log('Payload Received!')
+  console.log('User ID:', jwtPayload.id)
+  console.log('Token Expires On:', jwtPayload.exp)
+  
+  User.findById(jwtPayload.id)
+  .then(user => next(null, user))
+  .catch(() => next(null, false))
+})
+
+module.exports = strategy
