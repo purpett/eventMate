cf3a335a66f80cef3023fd4927145e8f1de0cfa6
Michela Bicocchi <mbicocchi1@gmail.com>
2023-05-10 21:25:46 +0100

    Change style to no colours, add CSS for sign up and log in page

--- eventmate-frontend/src/components/NavBar.js
+++ eventmate-frontend/src/components/NavBar.js
@@ -11,25 +11,28 @@ export default function NavBar() {
   }
 
   return (
-    <nav className='navbar'>
-      <Link to='/' id="logo">eventMate</Link>
-      <div className='nav-links-container'>
-        {tokenExp() &&
-          <div className=''>
-            <Link to='/CreateEventPage' className='nav-link'>Create Event</Link>
-            <Link to='/ProfilePage' className='nav-link'>My Profile</Link>
-            <button onClick={logOut} id='log-out-btn'> LOG OUT </button>
-          </div>
-        }
-        {!tokenExp() &&
-          <div>
-            <Link to='/Login' className='nav-link'>Sign In</Link>
-            <Link to='/SignUp' className='nav-link'>Sign Up</Link>
-          </div>
-        }
-      </div>
-    </nav>
+    <div className='navbar'>
+      <nav className='navbar-content'>
+        <div id="logo"><Link to='/'>eventMate</Link></div>
+        <div className='nav-links-container'>
+          {tokenExp() &&
+            <div className=''>
+              <Link to='/CreateEventPage' className='nav-link'>Create Event</Link>
+              <Link to='/ProfilePage' className='nav-link'>My Profile</Link>
+              <button onClick={logOut} id='log-out-btn'> LOG OUT </button>
+            </div>
+          }
+          {!tokenExp() &&
+            <div>
+              <Link to='/Login' className='nav-link'>Login</Link>
+              <Link to='/SignUp' className='nav-link'>Sign Up</Link>
+            </div>
+          }
+        </div>
+      </nav>
+      <hr />
+    </div>
   )
 }
- 
+
 
