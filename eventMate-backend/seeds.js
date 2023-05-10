618fbbf6716f36628ce496237cfcd5ac24155adc
Miles Morris <milesmorris123@gmail.com>
2023-05-10 19:58:32 +0100

    done step two of backend prep

--- eventMate-backend/seeds.js
+++ eventMate-backend/seeds.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
 const Event = require('./models/event')
 const User = require('./models/user')
-const dbConfig = require('./config/db');
+// const dbConfig = require('./config/db');
+
+const db = require('./config/db');
+
+// Establish Database Connection
+mongoose.connect(db, { useNewUrlParser: true });
+mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
 
 const firstEvent = new Event({
   title: 'Beatles in Concert',
@@ -54,7 +60,7 @@ firstUser.attending.push(secondEvent)
 
 const seedDatabase = async () => {
   try {
-  mongoose.connect(dbConfig)
+  mongoose.connect(db)
   await firstUser.save()
   await secondUser.save()
   await firstEvent.save()
--- eventmate-frontend/src/components/ProfilePage.js
+++ eventmate-frontend/src/components/ProfilePage.js
@@ -9,7 +9,7 @@ export default function ProfilePage() {
 
   const [userEvents, setUserEvents] = useState([])
   const [currentUser, setCurrentUser] = useState({ username: '', attending: [] })
-  const navigate = useNavigate()
+
 
   const navigate = useNavigate()
   // Load the getUser api specific to the user id on page load
