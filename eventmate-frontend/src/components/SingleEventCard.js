6695ca51f3b8858223b4d76bfaa1e9536831d121
Michela Bicocchi <mbicocchi1@gmail.com>
2023-05-11 16:14:53 +0100

    Add style for all events cards

--- eventmate-frontend/src/components/SingleEventCard.js
+++ eventmate-frontend/src/components/SingleEventCard.js
@@ -8,14 +8,23 @@ export default function SingleEventCard({ eventInfo, index }) {
       {/* Displays the information passed down from AllEventsPage. */}
       <Link to={`${eventInfo._id}`}>
         <div className="event-card">
-          <div>
-            <p>Title: {eventInfo.title}</p>
-            <p>Location: {eventInfo.location}</p>
-            {/* Transforms the date into a more appealing format */}
-            <p>Date: {transformDate(eventInfo.date)}</p>
+          {index === 0 && <div class="popular-event-banner">POPULAR EVENT</div>}
+          <div className="card-details">
+            <div>
+              <p className='title'>{eventInfo.title}</p>
+              <div className='where'>
+                <img src="/media/map-marker-radius.svg" alt="map marker icon" />
+                {eventInfo.location}
+              </div>
+              {/* Transforms the date into a more appealing format */}
+              <div className='when'>
+                <img src="/media/calendar-month-outline.svg" alt="calendar icon" />
+                {transformDate(eventInfo.date)}
+              </div>
+            </div>
+            {/* First event card will have additional info displayed on it */}
+            {index === 0 && <div className='most-attended'>{eventInfo.attendees.length} people are going to this event!</div>}
           </div>
-          {/* First event card will have additional info displayed on it */}
-          {index === 0 && <div className='most-attended'>{eventInfo.attendees.length} people are going to this event!</div>}
         </div>
       </Link>
       {/* Purely for testing purposes */}
