ceddf171a1ce7a172d9807f298b16c04ac5b0b8c
Miles Morris <milesmorris123@gmail.com>
2023-05-11 14:46:41 +0100

    commiting for deployment of backend

--- eventmate-frontend/src/apis/apiConfig.js
+++ eventmate-frontend/src/apis/apiConfig.js
@@ -2,7 +2,7 @@ let apiUrl;
 const expressPort = 5002;
 const apiUrls = {
   development: `http://localhost:${expressPort}/api`,
-  production: `https://example.domain.com/api`
+  production: `https://floating-chamber-62558.herokuapp.com/api`
 }
 
 if (window.location.hostname === 'localhost') {
