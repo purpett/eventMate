2398029fae6e70213456d8d28507dd2f1552356b
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-05-11 11:42:26 +0100

    Add so if you click on an event on the profile page it takes you to the event page

--- eventmate-frontend/src/components/UserEvent.js
+++ eventmate-frontend/src/components/UserEvent.js
@@ -1,11 +1,13 @@
 import transformDate from '../transformDate'
+import { Link } from 'react-router-dom'
 
 export default function UserEvent(props) {
   const { event } = props;
-  // Displays the users events on the page pretty straight forward stuff
+  // Displays the users events on the page pretty straight forward.
   return (
     <div className='event-card' id="user-event">
-      <span className='event-title'>{event.title}</span>
+      <Link to={`../${event._id}`}>
+      <span className='event-title'>{event.title}</span></Link>
       |
       <span>{transformDate(event.date)}</span>
     </div>
